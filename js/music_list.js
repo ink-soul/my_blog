92165beb7814503197c06a047ef11dafae5f0390
墨翎 <qingci30@163.com>
2021-04-01 08:07:52 +0800

    new music player

---
+++ js/music_list.js
@@ -0,0 +1,14 @@
+const ap = new APlayer({
+    container:document.getElementById('aplayer'),
+    fixed:true,
+    autoplay:true,
+    loop:"all",
+    
+    audio:[{
+        name:'龙脊雪山BGM',
+        artist:'童嘎',
+        url:'http://music.163.com/song/media/outer/url?id=1807478678.mp3',
+        cover:'https://cdn.jsdelivr.net/gh/ink-soul/blog_images/cover1.jpg'
+    }],
+    
+})
