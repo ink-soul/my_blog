c5b2992f7eb0bd6374ffa03978391df4d0c95922
墨翎 <qingci30@163.com>
2021-04-06 00:18:20 +0800

    Update music_list.js

--- js/music_list.js
+++ js/music_list.js
@@ -5,16 +5,30 @@ const ap = new APlayer({
     loop:"all",
     
     audio:[
+        {name:'皎洁的笑颜',
+        artist:'陈致逸/HoYO-Mix',
+        url:'http://music.163.com/song/media/outer/url?id=1833805540.mp3',
+        cover:'https://cdn.jsdelivr.net/gh/ink-soul/blog_images/%E5%8E%9F%E7%A5%9E-%E6%BC%A9%E6%B6%A1%E3%80%81%E8%90%BD%E6%98%9F%E4%B8%8E%E5%86%B0%E5%B1%B1%20%20Vortex%20of%20Legends.jpg'
+        },
+
         {
-            name:'皎洁的笑颜',
-            artist:'陈致逸',
-            url:'http://music.163.com/song/media/outer/url?id=1833805540.mp3',
+            name:'银白的往事',
+            artist:'陈致逸/HoYO-Mix',
+            url:'http://music.163.com/song/media/outer/url?id=1833806442.mp3',
             cover:'https://cdn.jsdelivr.net/gh/ink-soul/blog_images/%E5%8E%9F%E7%A5%9E-%E6%BC%A9%E6%B6%A1%E3%80%81%E8%90%BD%E6%98%9F%E4%B8%8E%E5%86%B0%E5%B1%B1%20%20Vortex%20of%20Legends.jpg'
+        
         },
 
 
-        
+
+
         ]
+        
+           
+
+
+        
+        
     
-})
+});
 
