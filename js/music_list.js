8911328f9437d31b3c77299ac2b9b63852d55a69
墨翎 <qingci30@163.com>
2021-04-05 23:56:22 +0800

    text write

--- js/music_list.js
+++ js/music_list.js
@@ -4,12 +4,17 @@ const ap = new APlayer({
     autoplay:true,
     loop:"all",
     
-    audio:[{
-        name:'龙脊雪山BGM',
-        artist:'童嘎',
-        url:'http://music.163.com/song/media/outer/url?id=1807478678.mp3',
-        cover:'https://cdn.jsdelivr.net/gh/ink-soul/blog_images/cover1.jpg'
-    }]
+    audio:[
+        {
+            name:'皎洁的笑颜',
+            artist:'陈致逸',
+            url:'http://music.163.com/song/media/outer/url?id=1833805540.mp3',
+            cover:'https://cdn.jsdelivr.net/gh/ink-soul/blog_images/%E5%8E%9F%E7%A5%9E-%E6%BC%A9%E6%B6%A1%E3%80%81%E8%90%BD%E6%98%9F%E4%B8%8E%E5%86%B0%E5%B1%B1%20%20Vortex%20of%20Legends.jpg'
+        },
+
+
+        
+        ]
     
 })
 
